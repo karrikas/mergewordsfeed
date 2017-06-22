8a47168ed5e28742baa58f3eb314886f38c8ed72
karrikas <karrikas@gmail.com>
2017-06-22 23:31:31 +0200

    feed update

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,11 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use AppBundle\Entity\TwitterConnect;
 
 class DefaultController extends Controller
 {
@@ -56,6 +61,65 @@ class DefaultController extends Controller
         }
 
         return $this->render('default/mixer.html.twig');
+    }
+
+    /**
+     * @Route("/connection", name="connection_to_twitter")
+     */
+    public function connectio2twitterAction(Request $request)
+    {
+        $em = $this->get('doctrine')->getManager();
+        $tc = new TwitterConnect();
+
+
+        $form = $this->createFormBuilder($tc)
+            ->add('access_token', TextType::class)
+            ->add('access_token_secret', TextType::class)
+            ->add('save', SubmitType::class)
+            ->getForm();
+
+         $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $tc = $form->getData();
+            $em->persist($tc);
+            $em->flush();
+
+            return $this->redirectToRoute('connection_to_twitter');
+        }
+
+        $twitterConnect = $em->getRepository('AppBundle:TwitterConnect')->findAll();
+
+        return $this->render('default/connection-to-twitter.html.twig', [
+            'form' => $form->createView(),
+            'twitterConnect' => $twitterConnect,
+        ]);
+    }
+
+    /**
+     * @Route("/connection/delete/{id}", name="connection_to_twitter_delete")
+     */
+    public function connectio2twitterDeleteAction(Request $request, $id)
+    {
+        $em = $this->get('doctrine')->getManager();
+        $connection = $em->getRepository('AppBundle:TwitterConnect')->find($id);
+        $em->remove($connection);
+        $em->flush();
+
+        return $this->redirectToRoute('connection_to_twitter');
+    }
+
+    /**
+     * @Route("/connection/test", name="connection_to_twitter_test")
+     */
+    public function connectio2twitterTestAction(Request $request)
+    {
+        $accessToken = $request->get('access_token');
+        $accessTokenSecret = $request->get('access_token_secret');
+
+        $test = $this->get('app.twitter')->testConnection($accessToken, $accessTokenSecret);
+
+        return new JsonResponse($test);
     }
 
     protected function mix($value)
